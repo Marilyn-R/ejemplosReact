2ff2bcaa822465ba4e76fc386f06d6a81540746d
Marilyn <marilyn20362@gmail.com>
2020-06-20 20:02:52 -0500

    actualizacion 1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+
+/* function Hello(props) {
+  console.log(props)
+  return (
+  <div id= "hello"> {props.mytext}
+  <h3>{props.subtitle}</h3>
+Compo</div>
+  )
+} */
+
+class Hello extends React.Component{
+
+  state ={
+    show: true
+  }
+
+  render (){
+    if (this.state.show){
+    return (
+    <div id= "hello"> {this.props.mytext}
+  <h3>{this.props.subtitle}</h3>
+Compo</div>
+    )
+  } else{
+    return <h1>no hay nada</h1>
+  }
+}
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     hj  <Hello mytext="Holi" subtitle="lorem ispum"/> <Hello mytext="holaa" subtitle="lorem ispum"/> <Hello mytext="holas"subtitle="lorem ispum"/></div>
   );
 }
 
