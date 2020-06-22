de748fa0219d12b4cb0e831335a317ceaccb4978
Marilyn <marilyn20362@gmail.com>
2020-06-21 23:27:42 -0500

    proyecto terminado de buscar

--- ejemplodos/src/App.js
+++ ejemplodos/src/App.js
@@ -1,47 +1,91 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import './App.css';
+
 import Buscador from './components/Buscador';
 import Resultado from './components/Resultado';
-
 class App extends Component {
-
-state={
-  termino : '',
-  imagenes : []
-}
-
-consultarApi = ()=>{
- 
-
-  const url= `https://pixabay.com/api/?key=17154799-8706e3c6f5433d0a0af8a31da&q=$(termino)`; 
   
-   fetch(url)
-  .then(respuesta => respuesta.json())
-  .then(resultado => this.setState({imagenes:resultado.hits}));
-}
+  constructor(){
+    super();
+    this.state={
+      termino:'',
+      imagenes: [],
+      pagina: ''
+    }
 
-datosBusqueda =(termino) =>{
-  this.setState({
-    termino
-  }, () =>{
-    this.consultarApi();
-  })
-}
+  }
+  
+  scroll =()=>{
+    const elemento= document.querySelector('.jumbotron');
+    elemento.scrollIntoView({block: "start", behavior: "smooth"});
+  }
 
-  render () {
-  return (
-    <div className="app container">
-      <div className="jumbotron">
-        <p className="lead text-center">
-          Buscador de productos </p>
-          <Buscador
+  paginaAnterior = ()=>{   
+    console.log("paginaAnterior");
+    let pagina= this.state.pagina;
+    if(pagina>1){
+      pagina-=1;
+      this.setState({pagina}, 
+        ()=>{
+          this.consultarApi();
+          this.scroll();
+        });
+    }
+    
+  }
+  paginaSiguiente = () => {
+    console.log("paginaSiguiente");
+    let pagina= this.state.pagina;
+    pagina++;
+    this.setState({pagina},
+      ()=>{this.consultarApi()
+        this.scroll();
+      });
+  }
+
+  consultarApi = ()=>{
+    const pagina= this.state.pagina;
+    const termino= this.state.termino;
+    const url= `https://pixabay.com/api/?key=1680832-74d43234194527aae929c2be5&q=${termino}&per_page=30&page=${pagina}`;
+    
+    fetch(url)
+    .then(respuesta => respuesta.json())
+    .then(resultado => this.setState({imagenes:resultado.hits}));
+  }
+
+  datosBusqueda = (termino) =>{
+    this.setState({
+      termino,
+      pagina:1
+    },()=>{
+      this.consultarApi();
+    })
+  }
+  
+
+  render(){
+    return (
+      <div className="app container">
+        <div className="jumbotron">
+          <p className="lead text-center">
+            Buscador de Imágenes
+          </p>
+          <Buscador 
           datosBusqueda={this.datosBusqueda}
           />
+  
+          <div className="row justify-content-center">
+            <Resultado 
+            imagenes={this.state.imagenes} 
+            paginaAnterior={this.paginaAnterior}
+            paginaSiguiente={this.paginaSiguiente}
+            />
+          </div>
+          
         </div>
-    <Resultado
-    imagenes ={this.state.imagenes}/>
       </div>
-    
-  );
+    );
   }
 }
+
 export default App;
--- ejemplodos/src/components/Buscador.js
+++ ejemplodos/src/components/Buscador.js
@@ -1,31 +1,24 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class Buscador extends Component{
-    
-busquedaRef = React.createRef();
 
-obtenerDatos =(e) =>{
-    e.preventDefault();
-
-    const termino = this.busquedaRef.current.value;
-    this.props.datosBusqueda(termino);  
-}
-    render (){
+    busquedaRef= React.createRef();
+    obtenerDatos = (e)=>{
+        e.preventDefault();
+        const termino = this.busquedaRef.current.value;
+        this.props.datosBusqueda(termino);
+    }
+    render(){
         return (
             <form onSubmit={this.obtenerDatos}>
+                
                 <div className="row">
-                    
                     <div className="form-group col-md-8">
-                        <input ref={this.busquedaRef} type="text" className="form-control
-                        form-control-lg" placeholder="Busca tu producto. 
-                        Ejemplo: Aretes morados"/>
+                        <input ref={this.busquedaRef} type="text" className="form-control form-control-lg" placeholder="Busca tu imagen. Ejemplo: Futbol"/>
                     </div>
-                    
                     <div className="form-group col-md-4">
-                        <input type="submit" className="btn btn-lg  btn-danger btn-block"
-                        value="Buscar..."/>
-                    </div>
-
+                        <input type="submit" className="btn btn--lg btn-block btn-info"  value="Buscar"/>
+                    </ div>
                 </div>
             </form>
         );
