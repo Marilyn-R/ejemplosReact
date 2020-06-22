dc435e71a3865915b343c5b54137a5d4481d87ed
Marilyn <marilyn20362@gmail.com>
2020-06-21 22:12:18 -0500

    actualizacion de proyecto

--- ejemplodos/src/App.js
+++ ejemplodos/src/App.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
 import Buscador from './components/Buscador';
-
+import Resultado from './components/Resultado';
 
 class App extends Component {
+
+state={
+  termino : '',
+  imagenes : []
+}
+
+consultarApi = ()=>{
+ 
+
+  const url= `https://pixabay.com/api/?key=17154799-8706e3c6f5433d0a0af8a31da&q=$(termino)`; 
+  
+   fetch(url)
+  .then(respuesta => respuesta.json())
+  .then(resultado => this.setState({imagenes:resultado.hits}));
+}
+
+datosBusqueda =(termino) =>{
+  this.setState({
+    termino
+  }, () =>{
+    this.consultarApi();
+  })
+}
+
   render () {
   return (
     <div className="app container">
       <div className="jumbotron">
-        <div className="lead text-center">
-          <p>Buscador de productos </p>
-          <Buscador/>
+        <p className="lead text-center">
+          Buscador de productos </p>
+          <Buscador
+          datosBusqueda={this.datosBusqueda}
+          />
         </div>
+    <Resultado
+    imagenes ={this.state.imagenes}/>
       </div>
-    </div>
+    
   );
   }
 }
--- ejemplodos/src/components/Buscador.js
+++ ejemplodos/src/components/Buscador.js
@@ -6,12 +6,15 @@ busquedaRef = React.createRef();
 
 obtenerDatos =(e) =>{
     e.preventDefault();
-    console.log(this.busquedaRef.current.value);
+
+    const termino = this.busquedaRef.current.value;
+    this.props.datosBusqueda(termino);  
 }
     render (){
         return (
             <form onSubmit={this.obtenerDatos}>
                 <div className="row">
+                    
                     <div className="form-group col-md-8">
                         <input ref={this.busquedaRef} type="text" className="form-control
                         form-control-lg" placeholder="Busca tu producto. 
